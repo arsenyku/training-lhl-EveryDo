7ef49a77433ce75158f9bf32a6a55bcca3d24c05
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-08 11:08:01 -0700

    Initial Commit

---
+++ Every Do/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Every Do
+//
+//  Created by asu on 2015-09-08.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
